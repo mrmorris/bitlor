c3970c00c64a2f30c3ede7e30c3d518426a3c7ea
Ryan Morris <mr.morris@gmail.com>
2020-07-14 07:14:26 -0400

    Include initial SQL

--- config/schema/initial.sql
+++ config/schema/initial.sql
@@ -16,4 +16,4 @@ CREATE TABLE `posts` (
   `created` timestamp NOT NULL,
   `modified` timestamp NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;s
