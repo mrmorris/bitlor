a2a4b8f0ba07777ef90c93c4721b557ff6be6bca
Ryan Morris <mr.morris@gmail.com>
2020-07-14 07:09:01 -0400

    Include initial SQL

---
+++ config/schema/initial.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `links` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `slug` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
+  `url` varchar(255) NOT NULL DEFAULT '',
+  `views` int NOT NULL DEFAULT '0',
+  `created` timestamp NOT NULL,
+  `modified` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug_idx` (`slug`),
+  UNIQUE KEY `url_idx` (`url`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `posts` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `body` text,
+  `created` timestamp NOT NULL,
+  `modified` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
